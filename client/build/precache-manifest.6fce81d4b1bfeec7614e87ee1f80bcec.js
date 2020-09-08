e8fb57c7d8041431eaeff928d53099b7a73c8171
Illya Antonuk <illantalex84@gmail.com>
2020-09-08 16:03:42 +0300

    Added build

---
+++ client/build/precache-manifest.6fce81d4b1bfeec7614e87ee1f80bcec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c79917572d297d015884549d728cd1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c920b41a85bd47c6976",
+    "url": "/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "55ddf90046f4e876e009",
+    "url": "/static/css/main.09901f06.chunk.css"
+  },
+  {
+    "revision": "7c920b41a85bd47c6976",
+    "url": "/static/js/2.d3c3b155.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/static/js/2.d3c3b155.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55ddf90046f4e876e009",
+    "url": "/static/js/main.51d4e891.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
