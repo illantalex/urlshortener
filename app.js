e33512d4ca33a61c0682820270725498c0385882
Illya Antonuk <illantalex84@gmail.com>
2020-09-08 16:25:23 +0300

    vsdbdfbd

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const PORT = config.get("port") || 5000;
 
 async function start() {
     try {
-        await mongoose.connect(await config.get("mongoUri"), {
+        await mongoose.connect(config.get("mongoUri"), {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
